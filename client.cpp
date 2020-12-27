351d05c79fef9b04f063e14c2bc4bf7d4e3e84e7
valerythf <65073104+valerythf@users.noreply.github.com>
2020-12-27 18:45:22 +0300

    Update and rename client-hw.cpp to client.cpp

--- client.cpp
+++ client.cpp
@@ -1,17 +1,14 @@
-#include <zmq.h>
-#include <iostream>
 #include "lib.cpp"
-
 
 int main() {
     Request_S ABSocket;
     ABSocket.socket_->bind("tcp://*:1111");
-    Publish_S counter_socket;
-    counter_socket.socket_->bind("tcp://*:2222");
+    Publish_S CountA;
+    CountA.socket_->bind("tcp://*:2222");
     std::string input;
     while (std::getline(std::cin, input)) {
         ABSocket.SendMessage(input, false);
-        counter_socket.SendMessage(std::to_string(input.size()),true);
+        CountA.SendMessage(std::to_string(input.size()),true);
         std::cout << ABSocket.ReceiveMessage() << "\n";
     }
     return 0;
